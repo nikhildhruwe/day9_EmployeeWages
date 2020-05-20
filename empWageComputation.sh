c96030e0d0e429f18497d6620c338e534652f9da
unknown <nick.dhruwe@gmail.com>
2020-05-20 21:47:41 +0530

    added limited wokring hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,6 +9,8 @@ absent=0
 fullTime=1
 partTime=2
 workingDays=20
+maxHours=100
+
 for ((day=1;$day<=$workingDays; day++))
 do
 empType=$((RANDOM%3))
@@ -20,8 +22,15 @@ case $empType in
 							*)		dailyHour=0
 							;;
 esac
+totalHours=$(($totalHours+$dailyHour))
 wagePerDay=$(($dailyHour*$wagePerHour))
 wagePerMonth=$(($wagePerMonth+$wagePerDay))
+if (($totalHours>=$maxHours))
+then
+	echo "Maximum Hours reached by employee"
+	break
+fi
 done
 echo " "
-echo "Monthly Wages : $wagePerMonth"
+echo "Monthly Wages      : $wagePerMonth"
+echo "Total Hours worked : $totalHours"
