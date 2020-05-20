567202ac438a364d91557f1340bb3a6ca6a31ada
unknown <nick.dhruwe@gmail.com>
2020-05-20 12:00:18 +0530

    added daily wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -x
+echo " "
+echo "    Welcome to Employee Wage Computation Program "
+echo " "
 
-echo " Welcome to Employee Wage Computation Program "
+wagePerHour=20
+dailyHour=8
 present=1
 absent=0
 attendance=$((RANDOM%2))
+
 if (( $attendance == $present ))
 then
 	echo "Employee Present"
+	dailyHour=8
 else
 	echo "Emmployee Absent"
+	dailyHour=0
 fi
+
+wagePerDay=$(($dailyHour*$wagePerHour))
+
+echo "Daily Employee Wage : $wagePerDay"
