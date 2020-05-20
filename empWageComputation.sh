d253645babdf0e4b0c9100c22d2a3e611dbaa6fd
unknown <nick.dhruwe@gmail.com>
2020-05-20 21:15:26 +0530

    wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,19 +8,23 @@ dailyHour=8
 absent=0
 fullTime=1
 partTime=2
+workingDays=20
+for ((day=1;$day<=$workingDays; day++))
+do
 empType=$((RANDOM%3))
-
 case $empType in
 				$fullTime)  	echo "Full Time Employee"
 									dailyHour=8
 						  ;;
  	 			$partTime ) 	echo "Part Time Employee:"
-									dailyHour=8
-									wagePerHour=10
+									dailyHour=4
 							;;
 							*)		echo "Employee is Absent"
 									dailyHour=0
 							;;
 esac
 wagePerDay=$(($dailyHour*$wagePerHour))
-echo "Employee Wage : $wagePerDay"
+wagePerMonth=$(($wagePerMonth+$wagePerDay))
+done
+echo " "
+echo "Monthly Wages : $wagePerMonth"
