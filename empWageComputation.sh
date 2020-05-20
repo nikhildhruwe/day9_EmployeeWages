9e66414b77cd443fe1904148a929256d0633050a
unknown <nick.dhruwe@gmail.com>
2020-05-20 17:44:13 +0530

    added switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,18 +10,17 @@ fullTime=1
 partTime=2
 empType=$((RANDOM%3))
 
-if (($empType == $fullTime))
-then
-	echo "Full Time Employee"
-	dailyHour=8
-elif (($empType == $partTime ))
-then
-	echo "Part Time Employee:"
-	dailyHour=8
-	wagePerHour=10
-else
-	echo "Employee is Absent"
-	dailyHour=0
-fi
+case $empType in
+				$fullTime)  	echo "Full Time Employee"
+									dailyHour=8
+						  ;;
+ 	 			$partTime ) 	echo "Part Time Employee:"
+									dailyHour=8
+									wagePerHour=10
+							;;
+							*)		echo "Employee is Absent"
+									dailyHour=0
+							;;
+esac
 wagePerDay=$(($dailyHour*$wagePerHour))
 echo "Employee Wage : $wagePerDay"
